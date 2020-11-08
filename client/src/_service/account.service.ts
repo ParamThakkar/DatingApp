9e58111105e7d20fcd9f4cdd61b5131683801230
Param Thakkar <thakkarparam.2000@gmail.com>
2020-11-08 19:09:38 +0530

    Register page added

---
+++ client/src/_service/account.service.ts
@@ -0,0 +1,39 @@
+import { User } from './../_models/user';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {map} from 'rxjs/operators';
+import { ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseURL = 'https://localhost:5001/api/';
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable()
+
+  constructor(private http:HttpClient) {
+
+   }
+
+   login(model:any)
+   {
+     return this.http.post(this.baseURL+'account/login',model).pipe(
+       map((response:User)=>{
+         var user = response;
+         localStorage.setItem('user',JSON.stringify(user));
+         this.currentUserSource.next(user);
+       })
+     );
+   }
+
+   setCurrentUser(user:User)
+   {
+     this.currentUserSource.next(user);
+   }
+
+   logout(){
+     localStorage.removeItem('user');
+     this.currentUserSource.next(null);
+   }
+}
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { AccountService } from './../_service/account.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/_models/user';
 
 @Component({
   selector: 'app-root',
@@ -11,20 +13,20 @@ export class AppComponent implements OnInit {
   title = 'The dating app';
   users:any;
 
-  constructor(private http:HttpClient)
+  constructor(private http:HttpClient,private accountService:AccountService)
   {
 
   }
   ngOnInit(): void {
-    this.getUsers();
+    
+    this.setCurrentUser();
   }
 
-  getUsers()
+  setCurrentUser()
   {
-    this.http.get("https://localhost:5001/api/users").subscribe(
-      response =>{
-        this.users = response;
-      }
-    )
+    const user:User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
+
+  
 }
